3e5f9901ced7deabe6853ab39e4e845e35e4d627
Lewis Nyman <hello@lewisnyman.co.uk>
2020-11-26 09:25:59 +0000

    Support all template engines
    I'm pretty sure that's all that needs to be done

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const readingTime = require('./lib/reading-time');
 
 module.exports = function(eleventyConfig, pluginNamespace) {
   eleventyConfig.namespace(pluginNamespace, () => {
-    eleventyConfig.addNunjucksFilter('readingTime', readingTime);
+    eleventyConfig.addFilter('readingTime', readingTime);
   });
 };
